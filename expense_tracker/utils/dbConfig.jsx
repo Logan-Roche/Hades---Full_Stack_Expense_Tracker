8be6c6a14f361e17817302e212c7693455820f20
Logan Roche <Ljroche04@gmail.com>
2024-06-18 00:42:55 -0700

    Work in Progress: Adding Expenses, Expenses page,

--- expense_tracker/utils/dbConfig.jsx
+++ expense_tracker/utils/dbConfig.jsx
@@ -2,4 +2,4 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 const sql = neon("REPLACE BEFORE STARTING");
-export const db = drizzle(sql,{schema});
+export const db = drizzle(sql, { schema });
